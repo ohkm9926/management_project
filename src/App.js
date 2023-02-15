1f96c29ef222784245e530cc0462fc2e8a7bb093
kyungmin <ohkm9926@naver.com>
2023-02-15 14:33:45 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='gray-background'>
+      <img src={logo} lat ="logo"/>
+      <h2>Let's develop management system!</h2>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
